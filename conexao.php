cf6a00a7585b73d07da466a6b3e29269c2dd2d4f
Nicole <rosanik3103@gmail.com>
2024-04-15 20:39:19 -0300

    conexão com banco de dados

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php 
+//Dados para conexÃ£o do banco
+$servidor = "localhost";
+$user = "root";
+$password = "root";
+$banco = "bd_sistema";
+
+//conectando no banco de dados utilizando a funÃ§Ã£o mysqli
+$conexao = new mysqli($servidor, $user, $password, $banco);
+
+?>
--- insert_user.php
+++ insert_user.php
@@ -1,17 +1,17 @@
 <?php
 
-
 $nome = $_POST['nome'];
-$setor = $_POST['senha'];
+$setor = $_POST['setor'];
 $login = $_POST['login'];
-$senha = $_POST['setor'];
+$senha = $_POST['senha'];
 
+include 'conexao.php';
 
+//Dados para inserir os dados
+$insert = "INSERT INTO tb_user VALUES (NULL,'$nome','$setor','$login','$senha')";
 
-echo "$nome";
-echo"<br>";
+$query = mysqli_query($conexao, $insert);
 
-
-
+echo "Inserido com sucesso";
 
 ?>
